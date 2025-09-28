3cc43b4cfbf5028bfad40c76bc99f2a871d16f4a
HVDevs <80708560+unknowncode44@users.noreply.github.com>
2025-09-27 22:09:21 -0300

    Fix Route
    Fixed unused route to "/".

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EjercicioUno from '@/components/ejercicios/EjercicioUno.vue'
 import EjercicioDos from '@/components/ejercicios/EjercicioDos.vue'
-import App from '@/App.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
